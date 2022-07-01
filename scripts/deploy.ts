cd729441e89cb335e7a2cc982d04ebfced32b451
William Cisotto <willgalindo@gmail.com>
2022-07-01 20:42:09 +0100

    deploy script implementation

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,29 +1,89 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
+import * as customBallotJson from "../artifacts/contracts/CustomBallot.sol/CustomBallot.json";
+import * as tokenJson from "../artifacts/contracts/Token.sol/MyToken.json";
 
-async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+const EXPOSED_KEY = "NOT_USED";
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
-
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+//Convert Strings to Bytes32 to save gas fee
+function convertStringArrayToBytes32(array: string[]) {
+  const bytes32Array = [];
+  for (let index = 0; index < array.length; index++) {
+    bytes32Array.push(ethers.utils.formatBytes32String(array[index]));
+  }
+  return bytes32Array;
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+async function main() {
+  const wallet =
+    process.env.MNEMONIC && process.env.MNEMONIC.length > 0
+      ? ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
+      : new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
+
+  console.log(`Using address ${wallet.address}`);
+  const provider = ethers.providers.getDefaultProvider("ropsten");
+  const signer = wallet.connect(provider);
+  const balanceBN = await signer.getBalance();
+  const balance = Number(ethers.utils.formatEther(balanceBN));
+  console.log(`Wallet balance ${balance}`);
+
+  //Throw error if connected wallet has under 0.01 ETH
+  if (balance < 0.01) {
+    throw new Error("Not enough ether");
+  }
+
+  console.log("Deploying Ballot contract");
+  console.log("Proposals: ");
+
+  const proposals = process.argv.slice(2);
+
+  //If length is under 2 no proposal is provided
+  if (proposals.length < 2) {
+    throw new Error("Not enough proposals provided");
+  }
+
+  //For each proposal display proposal name and index position
+  proposals.forEach((element, index) => {
+    console.log(`Proposal N. ${index + 1}: ${element}`);
+  });
+
+
+  //Deploy contract to blockchain
+  const tokenFactory = new ethers.ContractFactory(
+    tokenJson.abi, 
+    tokenJson.bytecode,
+    signer
+  );
+
+
+  const tokenContract = await tokenFactory.deploy();
+
+  console.log("Awaiting confirmations");
+  //Contract deployed
+  await tokenContract.deployed();
+
+  //Deploy contract to blockchain obtaining abi and bytecode from ballot.json
+  const customBallotFactory = new ethers.ContractFactory(
+    customBallotJson.abi,
+    customBallotJson.bytecode,
+    signer
+  );
+
+  //Create contract instance and pass proposals parameter in
+  const customBallotContract = await customBallotFactory.deploy(
+    convertStringArrayToBytes32(proposals),
+    tokenContract.address
+  );
+
+  console.log("Awaiting confirmations");
+  //Contract deployed
+  await customBallotContract.deployed();
+
+  console.log("Completed");
+  console.log(`Token Contract deployed at ${tokenContract.address}`);
+  console.log(`Custom Ballot Contract deployed at ${customBallotContract.address}`);
+  
+}
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
